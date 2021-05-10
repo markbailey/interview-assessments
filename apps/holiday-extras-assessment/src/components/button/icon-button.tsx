1d351fe5a67dc1e1f979c99e9bb674cc65eadd69
Mark Bailey <markbailey78@yahoo.co.uk>
2021-05-10 09:06:54 +0100

    Added asyncRetry function to fix issue of lazy loading components when not connection present

--- apps/holiday-extras-assessment/src/components/button/icon-button.tsx
+++ apps/holiday-extras-assessment/src/components/button/icon-button.tsx
@@ -1,19 +1,21 @@
 import { Suspense, lazy } from 'react';
 
+import asyncRetry from '../../utilities/asyncRetry';
 import IIconButtonProps from './interfaces/IIconButtonProps';
 import { IconButtonRoot } from './button.styled';
 
-const FlickrIcon = lazy(() => import('../icons/flickr'));
-const TopIcon = lazy(() => import('../icons/top'));
-const CloseIcon = lazy(() => import('../icons/close'));
+const FlickrIcon = lazy(() => asyncRetry(() => import('../icons/flickr')));
+const TopIcon = lazy(() => asyncRetry(() => import('../icons/top')));
+const CloseIcon = lazy(() => asyncRetry(() => import('../icons/close')));
 
 function IconButton({ icon, ...otherProps }: IIconButtonProps) {
   const render = (icon: string) => {
     const props = { size: 32, color: '#f5f5f5' };
+
     switch (icon) {
-      case 'top': return <TopIcon {...props} />;
-      case 'close': return <CloseIcon {...props} />;
-      default: return <FlickrIcon {...props} />;
+      case 'top': return TopIcon ? <TopIcon {...props} /> : null;
+      case 'close': return CloseIcon ? <CloseIcon {...props} /> : null;
+      default: return FlickrIcon ? <FlickrIcon {...props} /> : null;
     }
   }
 
--- apps/holiday-extras-assessment/src/components/icons/index.js
+++ apps/holiday-extras-assessment/src/components/icons/index.js
@@ -1,2 +1,3 @@
 export { default as FlickrIcon } from './flickr';
 export { default as SearchIcon } from './search';
+export { default as TopIcon } from './top';
