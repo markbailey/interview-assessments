a855c3f27fdc9520964cc74ab5e6bc7d83b9bd0e
Mark Bailey <markbailey78@yahoo.co.uk>
2021-05-10 09:18:43 +0100

    Removed lazy loading icons to fixed issues with lazy load failing when no connection

--- apps/holiday-extras-assessment/src/components/button/icon-button.tsx
+++ apps/holiday-extras-assessment/src/components/button/icon-button.tsx
@@ -1,29 +1,30 @@
-import { Suspense, lazy } from 'react';
+// import { Suspense, lazy } from 'react';
 
-import asyncRetry from '../../utilities/asyncRetry';
+// import asyncRetry from '../../utilities/asyncRetry';
 import IIconButtonProps from './interfaces/IIconButtonProps';
 import { IconButtonRoot } from './button.styled';
 
-const FlickrIcon = lazy(() => asyncRetry(() => import('../icons/flickr')));
-const TopIcon = lazy(() => asyncRetry(() => import('../icons/top')));
-const CloseIcon = lazy(() => asyncRetry(() => import('../icons/close')));
+import { FlickrIcon, TopIcon, CloseIcon } from '../icons';
+
+// const FlickrIcon = lazy(() => asyncRetry(() => import('../icons/flickr')));
+// const TopIcon = lazy(() => asyncRetry(() => import('../icons/top')));
+// const CloseIcon = lazy(() => asyncRetry(() => import('../icons/close')));
 
 function IconButton({ icon, ...otherProps }: IIconButtonProps) {
   const render = (icon: string) => {
     const props = { size: 32, color: '#f5f5f5' };
-
     switch (icon) {
-      case 'top': return TopIcon ? <TopIcon {...props} /> : null;
-      case 'close': return CloseIcon ? <CloseIcon {...props} /> : null;
-      default: return FlickrIcon ? <FlickrIcon {...props} /> : null;
+      case 'top': return <TopIcon {...props} />;
+      case 'close': return <CloseIcon {...props} />;
+      default: return <FlickrIcon {...props} />;
     }
   }
 
   return (
     <IconButtonRoot {...otherProps}>
-      <Suspense fallback={null}>
+      {/* <Suspense fallback={null}> */}
         {render(icon)}
-      </Suspense>
+      {/* </Suspense> */}
     </IconButtonRoot>
   )
 }
--- apps/holiday-extras-assessment/src/components/icons/index.js
+++ apps/holiday-extras-assessment/src/components/icons/index.js
@@ -1,3 +1,4 @@
 export { default as FlickrIcon } from './flickr';
+export { default as CloseIcon } from './close';
 export { default as SearchIcon } from './search';
 export { default as TopIcon } from './top';
