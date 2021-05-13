563edc980852f3e52b472cea3ef9c40bf8edccf2
Mark Bailey <markbailey78@yahoo.co.uk>
2021-05-13 21:33:42 +0100

    Update IPhotoMeta.ts

--- apps/holiday-extras-assessment/src/context/flickr/interfaces/IPhotoMeta.ts
+++ apps/holiday-extras-assessment/src/context/flickr/interfaces/IPhotoMeta.ts
@@ -17,15 +17,21 @@ interface IPhotoOwnerMeta {
   [p:string]: any;
 };
 
+interface IPhotoAuthorMeta {
+  id: string;
+  username: string;
+};
+
 interface IPhotoMeta {
   id: string;
-  title: IPhotoContentObject;// | string;
-  description: IPhotoContentObject;// | string;
-  owner: IPhotoOwnerMeta;
-  tags: IPhotoTags;// | string[];
+  title: IPhotoContentObject | string;
+  description: IPhotoContentObject | string;
+  owner?: IPhotoOwnerMeta;
+  author?: IPhotoAuthorMeta;
+  tags: IPhotoTags | string[];
   server: string;
   secret: string;
-  farm: number;
+  farm?: number;
   isfamily?: number;
   isfriend?: number;
   ispublic?: number;
